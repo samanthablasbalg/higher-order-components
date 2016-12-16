91fe92fedeb0cfd58aa3ec4317215ce683c46d19
samanthablasbalg <blasbalgs@gmail.com>
2016-12-16 10:11:31 -0500

    implement authentication

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Router, Route, browserHistory} from 'react-router';
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
+import requireAuth from './components/require_authentication';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -14,7 +15,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
     	<Route path="/" component={App}>
-    		<Route path="resources" component={Resources} />
+    		<Route path="resources" component={requireAuth(Resources)} />
     	</Route>
     </Router>
   </Provider>
